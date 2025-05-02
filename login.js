38fabca0d1ac14a30ab9452ff0f464f17b244ddf
zAyman01 <2301115@student.eelu.edu.eg>
2025-05-02 03:29:30 +0300

    Last edits

--- login.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector(".container");
-const loginlink= document.querySelector(".signinlink");
-const registerlink = document.querySelector(".signUplink");
-registerlink.addEventListener("click", () => {
-    container.classList.add("active");
-})
